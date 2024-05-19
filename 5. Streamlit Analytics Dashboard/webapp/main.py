5eb94dda7c104acb5ece26864ccbf0e8387b7d54
n1ml03 <antman6903nm@gmail.com>
2024-05-19 20:06:42 +0700

    commit

--- 5. Streamlit Analytics Dashboard/webapp/main.py
+++ 5. Streamlit Analytics Dashboard/webapp/main.py
@@ -23,7 +23,7 @@ def product_and_customer_behavior_analysis():
 
     # 1. Orders count across days of week and hourly comparison (heatmap)
     purchase_count = olist.groupby(['order_purchase_day', 'order_purchase_hour']).nunique()['order_id'].unstack()
-    st.markdown('Orders count across **days of week and hourly** comparison')
+    st.markdown('**Số lượng đơn hàng theo ngày trong tuần và giờ.**')
     fig, ax = plt.subplots(figsize=(20, 8))
     sns.heatmap(purchase_count.reindex(index=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']),
                 cmap="YlGnBu", annot=True, fmt="d", linewidths=0.2, ax=ax)
@@ -46,12 +46,10 @@ def product_and_customer_behavior_analysis():
     sales_per_purchase_month = olist.groupby(['order_purchase_month', 'order_purchase_mon', 'order_purchase_day'],
                                              as_index=False).payment_value.sum()
     sales_per_purchase_month = sales_per_purchase_month.sort_values(by=['order_purchase_month'], ascending=True)
-    st.markdown('Sales by **_Month_ _&_  _Day_ _of_ Week**.')
-    fig = px.line(sales_per_purchase_month, x="order_purchase_mon", y="payment_value", color='order_purchase_day',
-                  title='Sales Each Month & Each Day of Week')
-    fig.update_layout(title="Sales Each Month & Each Day of Week",
-                      xaxis_title="Months",
-                      yaxis_title="Sales (in $)",
+    fig = px.line(sales_per_purchase_month, x="order_purchase_mon", y="payment_value", color='order_purchase_day')
+    fig.update_layout(title="Doanh số theo tháng và ngày trong tuần.",
+                      xaxis_title="Tháng",
+                      yaxis_title="Doanh thu (USD)",
                       font=dict(family="Courier New, monospace", size=15, color="#7f7f7f"))
     st.write(fig)
 
@@ -75,12 +73,11 @@ def product_and_customer_behavior_analysis():
         {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov',
          12: 'Dec'})
     review_score_per_month = olist.groupby(['review_month', 'review_day'], as_index=False).review_score.mean()
-    st.markdown('**_Customer_ Rating** Each Month & Day of Week')
     fig = px.line(review_score_per_month, x="review_month", y="review_score", color='review_day',
-                  title='Ratings Each Month & Day of Week')
+                  title='Đánh giá của khách hàng theo từng tháng và ngày trong tuần')
     fig.update_layout(title="Ratings Each Month & Day of Week",
-                      xaxis_title="Months",
-                      yaxis_title="Review Ratings",
+                      xaxis_title="Tháng ",
+                      yaxis_title="Đánh giá xếp hạng",
                       font=dict(family="Courier New, monospace", size=15, color="#7f7f7f"))
     st.write(fig)
     st.markdown("""
@@ -97,9 +94,13 @@ def product_and_customer_behavior_analysis():
     avg_score_per_category = avg_score_per_category[avg_score_per_category['Number of Reviews'] > 100]
     avg_score_per_category = avg_score_per_category.sort_values(by='Number of Reviews', ascending=False)
     avg_ratings = avg_score_per_category[:20]
-    st.markdown('**_Average_ _Review_ Ratings** across Product categories')
     fig = px.bar(avg_ratings, x='Product Category', y='Number of Reviews',
-                 hover_data=['Average Review Ratings'], color='Average Review Ratings', height=500)
+                 hover_data=['Average Review Ratings'], color='Average Review Ratings', height=500,
+                 title='Đánh giá trung bình trên các loại sản phẩm')
+    fig.update_layout(
+        xaxis_title="Danh mục sản phẩm",
+        yaxis_title="Số lượng đánh giá",
+    )
     st.write(fig)
     st.markdown("""
     - Biểu đồ được sắp xếp theo thứ tự giảm dần về số lượng đánh giá, trong đó "Bed Bath & Table" có số lượng đánh giá cao nhất và "Consoles Games" có số lượng đánh giá thấp nhất trong số 20 danh mục hàng đầu. Điều này cho thấy mức độ tương tác và mức độ phổ biến khác nhau của khách hàng đối với các loại sản phẩm khác nhau.
@@ -115,13 +116,39 @@ def product_and_customer_behavior_analysis():
     total_rev_month = olist.groupby(['order_purchase_year', 'order_purchase_mon', 'product_category_name'],
                                     as_index=False).payment_value.sum()
     total_rev_month.columns = ['Sales Year', 'Sales Month', 'Product Category', 'Sales Revenue']
-    st.markdown('**_Product_ _Wise_ Revenue**')
-    fig = px.sunburst(total_rev_month, path=['Sales Year', 'Product Category'], values='Sales Revenue',
-                      color='Sales Revenue', hover_data=['Sales Revenue'],
-                      color_continuous_scale='RdBu',
-                      color_continuous_midpoint=np.average(total_rev_month['Sales Revenue'],
-                                                           weights=total_rev_month['Sales Revenue']))
+    top_products = total_rev_month.groupby('Product Category')['Sales Revenue'].sum().sort_values(ascending=False).head(
+        10).index
+
+    # Lọc dữ liệu cho 10 sản phẩm hàng đầu
+    top_data = total_rev_month[total_rev_month['Product Category'].isin(top_products)]
+
+    top_data = top_data.sort_values(by=['Sales Year', 'Product Category'])
+
+    fig = px.bar(
+        top_data,
+        x="Product Category",
+        y="Sales Revenue",
+        color="Sales Year",
+        barmode="group",  # Hiển thị cột ghép
+        title='Top 10 sản phẩm có tổng doanh thu cao nhất trong 3 năm'
+    )
+
+    # Tùy chỉnh bố cục
+    fig.update_layout(
+        xaxis_title="Loại sản phẩm",
+        yaxis_title="Doanh thu",
+        xaxis=dict(tickangle=-45),  # Xoay nhãn trục x
+    )
+
+    # Hiển thị chú thích (legend) ở bên phải
+    fig.update_layout(legend=dict(orientation="v", yanchor="top", y=1.02, xanchor="right", x=1))
+
+    # Hiển thị giá trị trên mỗi cột
+    fig.update_traces(texttemplate='%{y:.2f}', textposition='outside')
+
+    # Hiển thị biểu đồ
     st.write(fig)
+
     st.markdown("""
     Quan sát tổng thể:
     - Danh mục chiếm ưu thế: "Bed Bath & Table" và "Health & Beauty" liên tục chiếm ưu thế doanh số bán hàng trong cả năm 2017 và 2018, chiếm phân khúc lớn nhất trong cả hai năm.
@@ -135,12 +162,37 @@ def product_and_customer_behavior_analysis():
     total_rev_hour = olist[olist['order_purchase_year'] == 2018].groupby(
         ['order_purchase_hour', 'product_category_name'], as_index=False).payment_value.sum()
     total_rev_hour.columns = ['Sales Hour', 'Product Category', 'Sales Revenue']
-    st.markdown('Product Wise **_Revenue_ _by_ Hour**')
-    fig = px.sunburst(total_rev_hour, path=['Sales Hour', 'Product Category'], values='Sales Revenue',
-                      color='Sales Revenue', hover_data=['Product Category'],
-                      color_continuous_scale='RdBu',
-                      color_continuous_midpoint=np.average(total_rev_hour['Sales Revenue'],
-                                                           weights=total_rev_hour['Sales Revenue']))
+    # st.markdown('Product Wise **_Revenue_ _by_ Hour**')
+    # fig = px.sunburst(total_rev_hour, path=['Sales Hour', 'Product Category'], values='Sales Revenue',
+    #                   color='Sales Revenue', hover_data=['Product Category'],
+    #                   color_continuous_scale='RdBu',
+    #                   color_continuous_midpoint=np.average(total_rev_hour['Sales Revenue'],
+    #                                                        weights=total_rev_hour['Sales Revenue']))
+    # st.write(fig)
+
+    # Tính tổng doanh thu cho mỗi Product Category và chọn top 10
+    product_revenue = total_rev_hour.groupby("Product Category")["Sales Revenue"].sum()
+    top_n_products = product_revenue.nlargest(10).index
+
+    # Lọc dữ liệu chỉ lấy các sản phẩm trong top 10
+    filtered_data = total_rev_hour[total_rev_hour['Product Category'].isin(top_n_products)]
+
+    # Vẽ biểu đồ cột
+    fig = px.bar(
+        filtered_data,
+        x="Sales Hour",
+        y="Sales Revenue",
+        color="Product Category",
+        title='Doanh thu theo giờ của top 10 sản phẩm bán chạy nhất (2018)'
+    )
+
+    # Cập nhật bố cục
+    fig.update_layout(
+        xaxis_title="Giờ mua hàng",
+        yaxis_title="Doanh thu"
+    )
+
+    # Hiển thị biểu đồ
     st.write(fig)
     st.markdown("""
     Giờ cao điểm:
@@ -153,13 +205,39 @@ def product_and_customer_behavior_analysis():
     total_rev_dayofweek = olist[olist['order_purchase_year'] == 2018].groupby(
         ['order_purchase_day', 'product_category_name'], as_index=False).payment_value.sum()
     total_rev_dayofweek.columns = ['Sales DayofWeek', 'Product Category', 'Sales Revenue']
+    # st.markdown('**_Product_ _Wise_ Revenue** by Day of Week')
+    # fig = px.sunburst(total_rev_dayofweek, path=['Sales DayofWeek', 'Product Category'], values='Sales Revenue',
+    #                   color='Sales Revenue', hover_data=['Product Category'],
+    #                   color_continuous_scale='RdBu',
+    #                   color_continuous_midpoint=np.average(total_rev_dayofweek['Sales Revenue'],
+    #                                                        weights=total_rev_dayofweek['Sales Revenue']))
+    # fig.update_layout()
+    # st.write(fig)
+
+    # Tìm top N sản phẩm có doanh thu cao nhất
+    top_n_products = total_rev_dayofweek.groupby("Product Category")["Sales Revenue"].sum().nlargest(
+        10).index  # Ví dụ: lấy top 10 sản phẩm
+
+    # Lọc dữ liệu chỉ lấy các sản phẩm trong top N
+    filtered_data = total_rev_dayofweek[total_rev_dayofweek['Product Category'].isin(top_n_products)]
+
     st.markdown('**_Product_ _Wise_ Revenue** by Day of Week')
-    fig = px.sunburst(total_rev_dayofweek, path=['Sales DayofWeek', 'Product Category'], values='Sales Revenue',
-                      color='Sales Revenue', hover_data=['Product Category'],
-                      color_continuous_scale='RdBu',
-                      color_continuous_midpoint=np.average(total_rev_dayofweek['Sales Revenue'],
-                                                           weights=total_rev_dayofweek['Sales Revenue']))
-    fig.update_layout()  # Optional: Adjust layout if needed
+    fig = px.bar(
+        filtered_data,  # Sử dụng dữ liệu đã lọc
+        x="Sales DayofWeek",
+        y="Sales Revenue",
+        color="Product Category",
+        title=f'Doanh thu theo ngày trong tuần của top {len(top_n_products)} sản phẩm có doanh thu cao nhất (2018)'
+    )
+
+    # Tùy chỉnh bố cục
+    fig.update_layout(
+        xaxis_title="Ngày trong tuần",
+        yaxis_title="Doanh thu",
+        xaxis={'categoryorder': 'array',
+               'categoryarray': ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'Chủ nhật']}
+    )
+
     st.write(fig)
 
 
@@ -178,7 +256,7 @@ def customer_segmentation_and_ltv():
                            'rgba(255,22,84,1)']),
     )
     data = [trace0]
-    layout = go.Layout(title='Avg days between first and last purchase')
+    layout = go.Layout(title='Số ngày trung bình giữa lần mua đầu tiên và lần mua cuối cùng')
     fig = go.Figure(data=data, layout=layout)
     st.write(fig)
 
@@ -190,12 +268,12 @@ def customer_segmentation_and_ltv():
 
     # 2. RFM Customer Segmentation (treemap)
     rfm_level_ag = pd.read_csv('./data/rfm_level_ag.csv')
-    st.markdown('**_RFM_ _Customer_ Segmentation**')
     fig1 = go.Figure(go.Treemap(
         labels=rfm_level_ag['Customer Segment'],
         parents=['Customer Segmentation'] * len(rfm_level_ag),  # Create parent level for all segments
         values=rfm_level_ag['Monetary.1']
     ))
+    fig1.update_layout(title='Phân khúc khách hàng RFM')
     st.write(fig1)
 
     st.markdown("""
@@ -252,23 +330,22 @@ def customer_segmentation_and_ltv():
 
 
 def geography_analysis():
-    st.subheader('Geography Analysis')
+    # st.subheader('Geography Analysis')
     # st.success("Social Media is a whole new world, that's where the opportunity resides. Are you Analysing it?")
 
-    # 2. Customer Review and Other Geography-related Visualizations (images)
-    st.markdown('**Customer Review Across Brazil**')
+    st.markdown('**Đánh giá của khách hàng trên khắp Brazil**')
     image8 = Image.open('./images/mapplot.png')
     st.image(image8, use_column_width=True)
     st.markdown("""Bản đồ mật độ mã zip của Brazil cho thấy sự phân bố không đồng đều, với các cụm mật độ cao tập trung tại khu vực Đông Nam (São Paulo, Rio de Janeiro, Minas Gerais), khu vực phía Nam và dọc theo bờ biển Đông Bắc, phản ánh sự đô thị hóa và phát triển kinh tế tại những vùng này. Ngược lại, vùng Amazon và khu vực Trung Tây thể hiện mật độ thấp hơn do đặc điểm dân cư thưa thớt và kinh tế kém phát triển. Sự tập trung mã zip dọc theo bờ biển cũng làm nổi bật mô hình phát triển lịch sử và kinh tế của Brazil, nơi các thành phố ven biển đóng vai trò trung tâm thương mại và dân cư.
     """)
 
-    # st.markdown('**_Revenue_ Brazil** Across Brazil')
-    # image9 = Image.open('./images/revenue.png')
-    # st.image(image9, use_column_width=True)
-    # st.markdown("""
-    #
-    # """)
-    st.markdown('**E-Commerce on Brazilian State**')
+    st.markdown('**Thời gian giao hàng trung bình**')
+    image9 = Image.open('./images/average_delivery.png')
+    st.image(image9, use_column_width=True)
+    st.markdown("""
+
+    """)
+    st.markdown('**Thương mại điện tử ở bang Brazil**')
     image10 = Image.open('./images/best_state.png')
     st.image(image10, use_column_width=True)
     st.markdown("""
@@ -327,17 +404,29 @@ def sentiment():
     predictions = pd.read_csv("./data/sentiment_predictions.csv")
     predictions = predictions.dropna(subset=['review'])
 
-    # Allow users to search for reviews by product ID
+    # Xóa các dòng trùng lặp dựa trên cột 'review'
+    predictions = predictions.drop_duplicates(subset=['review'])
+
+    # Tính số lần xuất hiện của mỗi product_id
+    product_counts = predictions['product_id'].value_counts()
+
+    # Sắp xếp product_id theo thứ tự giảm dần
+    sorted_product_ids = product_counts.index.tolist()
+
+    # Thay đổi cách hiển thị
     st.header("Search Reviews by Product ID")
-    product_id = st.text_input("Enter Product ID:")
-    if product_id:
-        filtered_reviews = predictions[predictions['product_id'] == product_id][['review', 'sentiment', 'probability']]
+    selected_product_id = st.selectbox("Select Product ID:", sorted_product_ids)
+
+    # Lọc và hiển thị đánh giá dựa trên product_id đã chọn
+    if selected_product_id:
+        filtered_reviews = predictions[predictions['product_id'] == selected_product_id][
+            ['review', 'sentiment', 'probability']]
         if not filtered_reviews.empty:
-            st.markdown(f"### Reviews for ID: {product_id}")
+            st.markdown(f"### Reviews for ID: {selected_product_id}")
             st.dataframe(filtered_reviews.style.applymap(highlight_positive, subset=['sentiment']),
                          use_container_width=True)
         else:
-            st.write("No reviews found for the entered Product ID.")
+            st.write("No reviews found for the selected Product ID.")
 
     # Allow users to search for reviews by text input
     # st.header("Sentiment Review ")
@@ -355,11 +444,11 @@ def sentiment():
 
 # Main Streamlit App
 def main():
-    menu = ["Product & Customer Behaviour Analysis", "Customer Segmentation & LTV",
-            "Geography Analysis", "Recommendation System", "Review Sentiment"]
+    menu = ["Product & Customer Behaviour Analysis", "Geography Analysis"
+            , "Customer Segmentation & LTV","Recommendation System", "Review Sentiment"]
     choices = st.selectbox("Select Analytics Dashboard", menu)
 
-    st.title("Olist Analytics Dashboard - Marketing Matters!")
+    st.title("Analytics Dashboard!")
     st.markdown("""<style>body {    color: #fff;    background-color: #0A3648;}</style>    """, unsafe_allow_html=True)
 
     if choices == 'Product & Customer Behaviour Analysis':
